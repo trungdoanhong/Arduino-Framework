89cb4d2b3400de5c739c08b92964b7479b5d3810
trungdoanhong <trungdoanhong@gmail.com>
2016-11-30 16:02:56 +0700

    Multi Thread Library v1.2

---
+++ Multi_Thread_Example/Multi_Thread_Example.ino
@@ -0,0 +1,30 @@
+#include "MultiThread.h"
+
+MultiThread printSchedule = MultiThread();
+MultiThread printSchedule2 = MultiThread();
+
+int Counter = 0;
+
+void setup() {
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  
+  PrintEvery100();
+  PrintEvery1000();
+}
+
+void PrintEvery100()
+{
+  RUN_EVERY(printSchedule, 100)
+  Serial.println(Counter++);
+}
+
+void PrintEvery1000()
+{
+  RUN_EVERY(printSchedule2, 1000)
+  Serial.println("A");
+}
+
