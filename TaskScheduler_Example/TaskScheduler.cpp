711d5f0ef5f64ab9beb7dd47553fac3d92899763
trungdoanhong <trungdoanhong@gmail.com>
2017-05-17 09:47:02 +0700

    EDIT
    Reverse verson

--- TaskScheduler_Example/TaskScheduler.cpp
+++ TaskScheduler_Example/TaskScheduler.cpp
@@ -26,7 +26,7 @@ void TaskSchedulerClass::Init()
 void TaskSchedulerClass::Add(void(*func)(), uint16_t time)
 {
 	threadNumber++;		// initial number is 0 and increase 1 value before add and function into array
-	Thread *threadTemp = new Thread[threadNumber - 1];	// temporary array use to save olde thread array
+	Thread *threadTemp = new Thread[threadNumber];	// temporary array use to save olde thread array
 
 	for (uint8_t index = 0; index < (threadNumber - 1); index++)
 	{
@@ -38,17 +38,13 @@ void TaskSchedulerClass::Add(void(*func)(), uint16_t time)
 		delete[] ThreadArray;
 	}
 
-	ThreadArray = new Thread[threadNumber];
-	for (uint8_t index = 0; index < (threadNumber - 1); index++)
-	{
-		ThreadArray[index] = threadTemp[index];
-	}
-
+	ThreadArray = threadTemp;
+	
 	Thread thread;
 	thread.Func = func;
 	thread.Time = time;
 	thread.CountDown = 0;
-
+ 
 	// Add new thread into array
 	ThreadArray[threadNumber - 1] = thread;
 }
@@ -98,7 +94,8 @@ void TaskSchedulerClass::Execute()
 
 	timeCounter = 0;
 
-	ThreadArray[nearestThreadOrder].Func();
+	if (ThreadArray[nearestThreadOrder].Enough == true)
+		ThreadArray[nearestThreadOrder].Func();		// implement function
 
 	for (uint8_t index = 0; index < threadNumber; index++)
 	{
@@ -122,6 +119,88 @@ void TaskSchedulerClass::Execute()
 	// Set time to call function in next time
 	funcCallingTime = ThreadArray[nearestThreadOrder].CountDown;
 
+}
+
+void TaskSchedulerClass::Change(void(*func)(), uint16_t time)
+{
+	if (!this->IsFunctionExit(func))
+	{
+		this->Add(func, time);
+		return;
+	}
+		
+
+	for (uint8_t index = 0; index < threadNumber; index++)
+	{ 
+		if (ThreadArray[index].Func == func)
+		{
+			ThreadArray[index].Time = time;
+			ThreadArray[index].CountDown = time;
+		}
+	}
+}
+
+void TaskSchedulerClass::Stop(void(*func)())
+{
+	for (uint8_t index = 0; index < threadNumber; index++)
+	{
+		if (ThreadArray[index].Func == func)
+		{
+			ThreadArray[index].Enough = false;
+		}
+	}
+}
+
+void TaskSchedulerClass::Resum(void(*func)())
+{
+	for (uint8_t index = 0; index < threadNumber; index++)
+	{
+		if (ThreadArray[index].Func == func)
+		{
+			ThreadArray[index].Enough = true;
+		}
+	}
+}
+
+void TaskSchedulerClass::Delete(void(*func)())
+{
+	if (threadNumber > 0)
+
+	threadNumber--;	
+	Thread *threadTemp = new Thread[threadNumber];	// temporary array use to save olde thread array
+
+	uint8_t index1 = 0;
+	for (uint8_t index2 = 0; index2 < threadNumber + 1; index2++)
+	{
+		if (ThreadArray[index2].Func != func)
+		{
+			threadTemp[index1] = ThreadArray[index2];
+			index1++;
+		}
+	}
+
+	if (ThreadArray != NULL)
+	{
+		delete[] ThreadArray;
+	}
+
+	ThreadArray = new Thread[threadNumber];
+	for (uint8_t index = 0; index < threadNumber; index++)
+	{
+		ThreadArray[index] = threadTemp[index];
+	}
+}
+
+bool TaskSchedulerClass::IsFunctionExit(void(*func)())
+{
+	for ( uint8_t index = 0; index < threadNumber; index++ )
+	{
+		if (ThreadArray[index].Func == func)
+		{
+			return true;
+		}
+	}
+	return false;
 }
 
 ISR(TIMER2_OVF_vect)
